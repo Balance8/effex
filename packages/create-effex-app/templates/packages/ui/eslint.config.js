263fabc1d23e8d74266a035e3998799a9b1ccd20
Michael Slocum <mslocum819@gmail.com>
2025-10-15 21:37:15 -0700

    chore(create-effex-app): bump version to 0.1.11 and remove ui template

--- packages/create-effex-app/templates/packages/ui/eslint.config.js
+++
@@ -1,5 +0,0 @@
-// biome-ignore lint/style/noExportedImports: Required for ESLint config re-export
-import { config } from '@workspace/eslint-config/react-internal'
-
-/** @type {import("eslint").Linter.Config} */
-export default config
